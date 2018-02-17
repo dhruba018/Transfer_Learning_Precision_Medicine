eb6cc93e0f5f9518f757aa359f81f47a261832c4
dhruba018 <36573961+dhruba018@users.noreply.github.com>
2018-02-17 14:19:17 -0600

    Add files via upload

---
+++ LatentPredTransLearn.m
@@ -0,0 +1,117 @@
+function pPred = LatentPredTransLearn(pTrain, sTrain, pTest, sTest)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% FUNCTION
+%      pPred = LatentPredTransLearn(pTrain, sTrain, pTest, sTest)
+% Latent Variable Cost Optimization based Transfer Learning Prediction
+% models. Includes three prediction models --
+%      Latent Regression Prediction (LRP) 
+%      Latent-Latent Prediction (LLP) 
+%      Combined Latent Prediction (CLP) 
+%
+% INPUT ARGUMENTS:
+%      pTrain:  n1 x (p+1) matrix of primary training data. First p columns
+%                   contain training input Xp_train. Last column is the
+%                   training response yp_train.
+%      sTrain:  n1 x (p+1) matrix of secondary training data. First p
+%                   columns contain training input Xs_train. Last column
+%                   is the training response ys_train.
+%      pTest:   n2 x p matrix of primary test data. Contains test input
+%                   Xp_test.
+%      sTest:   n2 x (p+1) matrix of secondary test data. First p columns
+%                  contain test input Xs_test. Last column is the test
+%                  response ys_test.
+% 
+% OUTPUT ARGUMENTS:
+%      pPred:  n2 x 3 matrix of primary prediction output. First column
+%                  contains the LRP prediction yp_pred_LRP, second column
+%                  contains the LLP prediction, yp_pred_LLP & the third
+%                  column contains the CLP prediction, yp_pred_CLP.
+%
+% (c) 2017 S. R. Dhruba
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% pPred = LatentPredTransLearn(pTrain, sTrain, pTest, sTest)
+% Inputs...
+X11 = pTrain(:, 1:end-1);           X12 = pTest;                         % Primary X
+X21 = sTrain(:, 1:end-1);           X22 = sTest(:, 1:end-1);        % Secondary X
+y11 = pTrain(:, end);                                                             % Primary y
+y21 = sTrain(:, end);                 y22 = sTest(:, end);              % Secondary y
+X1 = [X11; X12];                        X2 = [X21; X22];
+[n, p] = size(X1);
+nTrain = size(X11, 1);                nTest = size(X12, 1);
+
+% Cost optimization...
+A = [ ];    b = [ ];    Aeq = [0, 1, 1];      beq = 1;
+lb = [-1, 0, 0]';   ub = [1, 1, 1]';
+options = optimoptions(@fmincon, 'display', 'off');
+
+% Response... y11 & y21
+fprintf('>> Cost optimization of response data... ')
+J = @(c) OptFuncTL(y11, y21, c);        c0 = [-1, 0, 1]';
+c_opt = fmincon(J, c0, A, b, Aeq, beq, lb, ub, [ ], options);
+[~, w1, a1_opt, a2_opt] = OptFuncTL(y11, y21, c_opt);                 % w = c0 + c1*y1 + c2*y2
+fprintf('Finished! \n')
+
+% Predictors... X1 & X2
+fprintf('>> Cost optimization of input data... ')
+L_opt = zeros(3, p);                                               % ? = [?1, ?2, ... , ?p]
+V = zeros(n, p);
+for k = 1:p
+    x1k = X1(:, k);     x2k = X2(:, k);                          % x_{1k}, x_{2k}
+    Jk = @(Lk) OptFuncTL(x1k, x2k, Lk);         Lk0 = [-1, 0, 1]';
+    Lk_opt = fmincon(Jk, Lk0, A, b, Aeq, beq, lb, ub, [ ], options);       % ?_k
+    L_opt(:, k) = Lk_opt;
+    [~, vk] = OptFuncTL(x1k, x2k, Lk_opt);                % v_k = ?k_0 + ?k_1 x_{1k} + ?k_2 x_{2k} 
+    V(:, k) = vk;
+end
+V1 = V(1:nTrain, :);     V2 = V(nTrain+1:end, :);
+fprintf('Finished! \n')
+
+% LATENT REGRESSION PREDICTION (LRP)...
+fprintf('>> Performing LRP... \n')
+Y21 = [ones(nTrain, 1), y21];
+Y22 = [ones(nTest, 1), y22];
+b2 = pinv(Y21)*w1;                                              % w1 = Y21*b2
+w2_LRP = Y22*b2;                                              % w2 = Y22*b2
+W2_LRP = [ones(nTest, 1), w2_LRP];
+y12_LRP = W2_LRP*a1_opt;                                 % Final prediction => y12 = a10 + a11*w2
+y12_LRP(y12_LRP < 0) = 0;
+
+% LATENT-LATENT PREDICTION (LLP)...
+fprintf('>> Performing LLP... \n')
+n_tree = 100;       Train_X = V1;      Train_y = w1;        Test_X = V2;
+M = TreeBagger(n_tree, Train_X, Train_y, 'method', 'regression');
+Pred_y = predict(M, Test_X);
+w2_LLP = Pred_y;                                                % Predicted latent vector
+W2_LLP = [ones(nTest, 1), w2_LLP];
+y12_LLP = W2_LLP*a1_opt;                                  % Final prediction => y12 = a10 + a11*w2
+y12_LLP(y12_LLP < 0) = 0;
+
+% COMBINED LATENT PREDICTION (CLP)...
+fprintf('>> Performing CLP... \n')
+w2_CLP = (w2_LLP + w2_LRP)/2;
+W2_CLP = [ones(size(w2_CLP)), w2_CLP];
+y12_CLP = W2_CLP*a1_opt;                                 % Final prediction => y12 = a10 + a11*w2
+y12_CLP(y12_CLP < 0) = 0;
+
+% Output...
+pPred = [y12_LRP, y12_LLP, y12_CLP];
+fprintf('>>\n >> Finished!!! \n')
+end
+
+
+% Cost Function...
+function [J, u, a1, a2] = OptFuncTL(v1, v2, w)
+u = [ones(size(v1)), v1, v2]*w;                               % u = w0 + w1*v1 + w2*v2
+U = [ones(size(u)), u];
+a1 = pinv(U)*v1;
+a2 = pinv(U)*v2;
+
+v1_hat = U*a1;      v1_hat(v1_hat < 0) = 0;
+v2_hat = U*a2;     v2_hat(v2_hat < 0) = 0;
+J = (norm(v1 - v1_hat)^2 + norm(v2 - v2_hat)^2) / (corr(v1, u) + corr(v2, u));
+end
+
+
+
---
+++ MappingTransLearn.m
@@ -0,0 +1,175 @@
+function pPred = MappingTransLearn(pTrain, sTrain, pTest, sTest)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% FUNCTION
+%      pPred = MappingTransLearn(pTrain, sTrain, pTest, sTest)
+% Nonlinear Mapping based Transfer Learning Prediction models, where the
+% input & output data in primary are mapped to the secondary space using
+% polynomial regression mapping.
+%
+% INPUT ARGUMENTS:
+%      pTrain:  n1 x (p+1) matrix of primary training data. First p columns
+%                   contain training input Xp_train. Last column is the
+%                   training response yp_train.
+%      sTrain:  n1 x (p+1) matrix of secondary training data. First p
+%                   columns contain training input Xs_train. Last column
+%                   is the training response ys_train.
+%      pTest:   n2 x p matrix of primary test data. Contains test input
+%                   Xp_test.
+%      sTest:   n2 x (p+1) matrix of secondary test data. First p columns
+%                  contain test input Xs_test. Last column is the test
+%                  response ys_test.
+% 
+% OUTPUT ARGUMENTS:
+%      pPred:  n2 x 1 matrix of primary prediction output.
+%
+% (c) 2017 S. R. Dhruba
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% pPred = MappingTransLearn(pTrain, sTrain, pTest, sTest)
+% Inputs...
+G11 = pTrain(:, 1:end-1);           G12 = pTest;                         % Primary X
+G21 = sTrain(:, 1:end-1);           G22 = sTest(:, 1:end-1);         % Secondary X
+d11 = pTrain(:, end);                                                             % Primary y
+d21 = sTrain(:, end);                d22 = sTest(:, end);               % Secondary y
+G1 = [G11; G12];                       G2 = [G21; G22];            d2 = [d21; d22];
+[n, p] = size(G1);
+nTrain = size(G11, 1);               nTest = size(G12, 1);
+
+
+% % Input Mapping...
+%       x2 = h(x1) = a0 + a1*x1 = X1*A
+fprintf('>> Retrieving input mapping... ')
+nX = 1;
+H12 = zeros(nX+1, p);                                           % Mapping
+for j = 1:p
+    G11A = ones(nTrain, nX+1);                               % G1 = [1, g1]
+    for k = 1:nX
+        G11A(:, k+1) = G11(:, j).^k;
+    end
+    H12(:, j) = pinv(G11A) * G21(:, j);                     % A = pinv(G1)*g2
+end
+fprintf('Finished! \n')
+
+% Map to secondary...
+fprintf('>> Mapping test input... ')
+G22m = zeros(nTest, p);                                       % Mapped gx
+for j = 1:p
+    G12A = ones(nTest, nX+1);
+    for k = 1:nX
+        G12A(:, k+1) = G12(:, j).^k;
+    end
+    G22m(:, j) = G12A * H12(:, j);                          % g2 = G1*A
+end
+fprintf('Finished! \n')
+
+
+% % Response Mapping...
+%       y1 = f(y2) = b0 + b1*y2 + b2*y2^2 = Y2*B
+fprintf('>> Retrieving response mapping... ')
+nY = 2;
+D21 = ones(nTrain, nY+1);                                     % D2 = [1, d2, d2^2]
+D22 = ones(nTest, nY+1);
+for k = 1:nY
+    D21(:, k+1) = d21.^k;
+    D22(:, k+1) = d22.^k;
+end
+F21 = pinv(D21) * d11;                                          % B = pinv(D2)*d1
+% d12m = D22*F21;                                                 % y1 = Y2*B
+fprintf('Finished! \n')
+
+
+% % BIAS CORRECTION...
+% Validation set...
+fprintf('>> Retrieving Bias Model:Started... \n')
+rng(0)
+nVal = round(nTrain/2);
+idx1 = sort(randperm(nTrain, nVal))';
+idx2 = (1:nTrain)';             idx2(idx1) = [ ];
+val_G11 = G11(idx1, :);        val_G12 = G11(idx2, :);
+val_G21 = G21(idx1, :);       val_G22 = G21(idx2, :);
+val_d11 = d11(idx1);            val_d12 = d11(idx2, :);
+val_d21 = d21(idx1);           val_d22 = d21(idx2, :);
+
+fprintf('>> \t Inner MP loop... \n')
+% MP inner recursion for Bias model...
+% Input map...
+h12 = zeros(nX+1, p);
+for j = 1:p
+    val_G11A = ones(size(val_G11, 1), nX+1);           % G1 = [1, g1]
+    for k = 1:nX
+        val_G11A(:, k+1) = val_G11(:, j).^k;
+    end
+    h12(:, j) = pinv(val_G11A) * val_G21(:, j);          % A = pinv(G1)*g2
+end
+
+val_G21m = zeros(size(val_G12));
+for j = 1:p
+    val_G12A = ones(size(val_G12, 1), nX+1);
+    for k = 1:nX
+        val_G12A(:, k+1) = val_G12(:, j).^k;
+    end
+    val_G21m(:, j) = val_G12A * h12(:, j);               % g2 = G1*A
+end
+
+% Response map...
+val_D21 = ones(size(val_d21, 1), nY+1);                % D2 = [1, d2, d2^2]
+for k = 1:nY
+    val_D21(:, k+1) = val_d21.^k;
+end
+f21 = pinv(val_D21) * val_d11;                              % B = pinv(D2)*d1
+
+
+% % Prediction model training w/ secondary...
+fprintf('>> \t Training Prediction Model... ')
+n_tree = 100;       Train_X2 = G2;      Train_y2 = d2;
+M = TreeBagger(n_tree, Train_X2, Train_y2, 'method', 'regression');
+fprintf('Finished! \n')
+
+
+% Prediction for validation set...
+Valid_X21 = val_G21m;
+Pred_y21 = predict(M, Valid_X21);
+val_d22m = Pred_y21;
+
+% Map to original...
+val_D22m = ones(size(val_d22m, 1), nY+1);           % D2 = [1, d2, d2^2]
+for k = 1:nY
+    val_D22m(:, k+1) = val_d22m.^k;
+end
+val_d12m = val_D22m*f21;                                   % d1 = D2*B
+
+% Learning Bias Model...
+Train_X_bias = [val_G12, val_d12];     Train_y_bias = val_d12 - val_d12m;
+M_bias = TreeBagger(n_tree, Train_X_bias, Train_y_bias, 'method', 'regression');
+fprintf('>> Finished! \n')
+
+
+% Mapped Prediction...
+fprintf('>> Performing MP... \n')
+Test_X21 = G22m;
+Pred_y21 = predict(M, Test_X21);
+d22m = Pred_y21;
+
+% Map to Original Space...
+fprintf('>> \t Mapping back to Primary Domain! \n')
+D22m = ones(size(d22m, 1), nY+1);                        % D2 = [1, d2, d2^2]
+for k = 1:nY
+    D22m(:, k+1) = d22m.^k;
+end
+d12m = D22m*F21;                                               % d1 = D2*B
+
+fprintf('>> \t Estimating Bias! \n')
+% Estimating Bias...
+Test_X_bias = [G12, d12m];
+Pred_y_bias = predict(M_bias, Test_X_bias);
+d12b = Pred_y_bias;                                            % Bias Estimate
+
+% Bias corrected prediction...
+fprintf('>> \t Performing Bias Corrected Prediction \n')
+d12mb = d12m + d12b;
+pPred = d12mb;
+fprintf('>> Finished! \n')
+
+
+
