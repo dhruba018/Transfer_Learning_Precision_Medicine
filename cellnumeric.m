f80d964cd67d97c16b1db1c83501c7e8a3e80caf
SR Dhruba <36573961+dhruba018@users.noreply.github.com>
2020-12-08 00:28:22 -0600

    Delete cellnumeric.m

--- cellnumeric.m
+++
@@ -1,8 +0,0 @@
-function index = cellnumeric(cellarray)
-    index = [ ];
-    for k = 1:length(cellarray)
-        if isnumeric(cellarray{k})
-            index = [index; k];
-        end
-    end
-end
