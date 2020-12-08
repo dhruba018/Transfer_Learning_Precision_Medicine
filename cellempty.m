c17680324eb298d50390d4c39046ba91d9e2224b
SR Dhruba <36573961+dhruba018@users.noreply.github.com>
2020-12-08 00:28:08 -0600

    Delete cellempty.m

--- cellempty.m
+++
@@ -1,8 +0,0 @@
-function index = cellempty(cellarray)
-    index = [ ];
-    for k = 1:length(cellarray)
-        if isempty(cellarray{k})
-            index = [index; k];
-        end
-    end
-end
